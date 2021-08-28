7aa09437f6ae6aaae7b0c186099ae4f2483e4ef9
Avisa-SEI <aap571420@gmail.com>
2021-08-28 15:56:33 -0700

    wip: database setup

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import express from "express";
 import routes from "./src/routes/crmRoute";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 4000;
 
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/CRMdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: false,
+});
+
+// bodyparser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 routes(app);
 
 app.get("/", (req, res) =>
