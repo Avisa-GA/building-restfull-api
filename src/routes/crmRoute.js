2365896cd256b2869c957ece0cc8f61996ab2190
Avisa-SEI <aap571420@gmail.com>
2021-08-29 09:39:33 -0700

    feat: create specific ID GET endpoint

--- src/routes/crmRoute.js
+++ src/routes/crmRoute.js
@@ -1,10 +1,15 @@
-import { addNewContact, getContacts } from "../controllers/crmController";
+import {
+  addNewContact,
+  getContacts,
+  getContactsWithID,
+} from "../controllers/crmController";
 
 const routes = (app) => {
   app.route("/contact").get(getContacts).post(addNewContact);
 
   app
     .route("/contact/:contactID")
+    .get(getContactsWithID)
     .put((req, res) => res.send("PUT request successfull"))
     .delete((req, res) => res.send("DELETE request successfull"));
 };
