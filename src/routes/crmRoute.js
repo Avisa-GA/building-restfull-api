17fcd80c22d31df01c5f35e8d84644390a2be460
Avisa-SEI <aap571420@gmail.com>
2021-08-29 09:04:28 -0700

    wip: add getContacts func to the route

--- src/routes/crmRoute.js
+++ src/routes/crmRoute.js
@@ -1,20 +1,7 @@
-import { addNewContact } from "../controllers/crmController";
+import { addNewContact, getContacts } from "../controllers/crmController";
 
 const routes = (app) => {
-  app
-    .route("/contact")
-    .get(
-      (req, res, next) => {
-        // middleware
-        console.log(`Request from: ${req.originalUrl}`);
-        console.log(`Request type: ${req.method}`);
-        next();
-      },
-      (req, res, next) => {
-        res.send("GET request successful");
-      }
-    )
-    .post(addNewContact);
+  app.route("/contact").get(getContacts).post(addNewContact);
 
   app
     .route("/contact/:contactID")
