f5e773994ed4b7045bff5a3c17b4db2704a8511f
Avisa-SEI <aap571420@gmail.com>
2021-08-29 13:48:51 -0700

    feat: add handlers for login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,7 +5,7 @@ import { UserSchema } from "../models/userModel";
 
 const User = mongoose.model("User", UserSchema);
 
-export const login = (req, res, next) => {
+export const loginRequired = (req, res, next) => {
   if (req.user) {
     next();
   } else {
@@ -24,4 +24,37 @@ export const register = (req, res) => {
       return res.json(user);
     }
   });
+};
+
+export const login = (req, res) => {
+  User.findOne(
+    {
+      email: req.body.email,
+    },
+    (err, user) => {
+      if (err) throw err;
+      if (!user) {
+        res
+          .status(401)
+          .json({ message: "Authentication failed. No user found" });
+      } else if (user) {
+        if (!user.comparePassword(req.body.hashPassword, user.hashPassword)) {
+          res
+            .status(401)
+            .json({ message: "Authentication failed. Wrong password!" });
+        } else {
+          return res.json({
+            token: jwt.sign(
+              {
+                email: user.email,
+                username: user.username,
+                _id: user.id,
+              },
+              "RESTFULAPIs"
+            ),
+          });
+        }
+      }
+    }
+  );
 };
